22f486b35258170da49b5da04799bc7af5d35b8d
claudio <claudiooartista@hotmail.co.uk>
2019-12-16 14:30:44 +0000

    Exception Handling with Java

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 _Read me file is main for me to take notes of the continuation of the project!_
 
 ##
-_TODO: Connect to Java Database and make queries to the database!
 This project is a combination of Selenium Cucumber with Selenium and Java from youTube Raghav Pal
 The WebDriverManager class is the a side project I decide to do for fun and practice!!_
 ##
+
+_TODO: Connect to Java Database and make queries to the database!_
 
 ##TestNG
 This lecturer covers how to add test annotations!
---
+++ src/test/java/demo/ExceptionHandlingDemo.java
@@ -0,0 +1,32 @@
+package demo;
+
+public class ExceptionHandlingDemo {
+        /*
+        /What is exception handling?
+        Exception unexpected condition at runtime
+        Exception Handling
+        Handling any unexpected behavior during execution
+        Catch the exception and log messages for troubleshooting
+        Enables continuation of runs even if a test fails
+        Practical Examples in the link below
+        https://www.youtube.com/results?search_query=%23ExceptionHandlingInJava
+         */
+        public static void main(String[] args) {
+
+            try {
+                System.out.println("Testing Exception handling");
+                int i = 1/0;
+                System.out.println("Test Completed");
+            }
+            catch (Exception exp){
+                System.out.println("I am inside the try block");
+                System.out.println("Message is " +exp.getMessage());
+                System.out.println("Cause is " +exp.getCause());
+                exp.printStackTrace();
+            }
+            // will always execute irrespective of an exception or not
+            finally {
+                System.out.println("I am inside the finally block");
+            }
+        }
+    }
