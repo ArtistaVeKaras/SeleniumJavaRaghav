7846b5aaf6b811ced8b2cdd29f4f3c25acae5e03
claudio <claudiooartista@hotmail.co.uk>
2019-12-20 11:55:42 +0000

    TestNG ignore test ok

---
+++ src/test/java/demo1/TestNGIgnoreDemo.java
@@ -0,0 +1,19 @@
+package demo1;
+
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+//This will ignore all test at the class level. No test should run!!
+@Ignore
+public class TestNGIgnoreDemo {
+
+    @Test
+    public void test1(){
+        System.out.print("This is the Test one ");
+    }
+
+    @Test
+    public void test2(){
+        System.out.print("This is the Test two ");
+    }
+}
